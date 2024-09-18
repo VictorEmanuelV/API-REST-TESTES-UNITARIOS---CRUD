7f83a32fd429adabc94c88f1accf077ab6b3f3bb
Victor Emanuel <Victor emanuel Vieira De Macedo>
2024-09-18 07:43:43 -0300

    userService class findById

---
+++ jUnit-5-e-Mockito/src/main/java/api_rest_mockito/jUnit5/e/Mockito/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package api_rest_mockito.jUnit5.e.Mockito.service.impl;
+
+import api_rest_mockito.jUnit5.e.Mockito.entity.User;
+import api_rest_mockito.jUnit5.e.Mockito.repository.UserRepository;
+import api_rest_mockito.jUnit5.e.Mockito.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public User findByid(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+}
