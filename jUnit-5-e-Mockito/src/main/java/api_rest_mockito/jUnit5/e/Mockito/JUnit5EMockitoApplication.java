4b745dfbd0b7c9593b87955173ac3a1a40e9cbce
Victor Emanuel <Victor emanuel Vieira De Macedo>
2024-09-18 06:56:36 -0300

    iniciando o projeto

---
+++ jUnit-5-e-Mockito/src/main/java/api_rest_mockito/jUnit5/e/Mockito/JUnit5EMockitoApplication.java
@@ -0,0 +1,13 @@
+package api_rest_mockito.jUnit5.e.Mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit5EMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnit5EMockitoApplication.class, args);
+	}
+
+}
---
+++ jUnit-5-e-Mockito/src/test/java/api_rest_mockito/jUnit5/e/Mockito/JUnit5EMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package api_rest_mockito.jUnit5.e.Mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnit5EMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
