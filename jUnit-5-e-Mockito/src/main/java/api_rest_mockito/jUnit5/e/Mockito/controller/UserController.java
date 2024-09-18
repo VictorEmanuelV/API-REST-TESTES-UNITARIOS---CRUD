b927f79df680776ecf27733b0bf620a558c5fa64
Victor Emanuel <Victor emanuel Vieira De Macedo>
2024-09-18 07:42:45 -0300

    user controller findbyid

--- jUnit-5-e-Mockito/src/main/java/api_rest_mockito/jUnit5/e/Mockito/controller/UserController.java
+++ jUnit-5-e-Mockito/src/main/java/api_rest_mockito/jUnit5/e/Mockito/controller/UserController.java
@@ -1,2 +1,20 @@
-package api_rest_mockito.jUnit5.e.Mockito.controller;public class UserController {
+package api_rest_mockito.jUnit5.e.Mockito.controller;
+import api_rest_mockito.jUnit5.e.Mockito.entity.User;
+import api_rest_mockito.jUnit5.e.Mockito.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User>findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(userService.findByid(id));
+    }
 }
