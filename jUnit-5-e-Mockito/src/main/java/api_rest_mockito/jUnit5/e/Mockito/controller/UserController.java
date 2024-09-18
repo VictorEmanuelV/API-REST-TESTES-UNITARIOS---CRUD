fee3bbe29eb15ac756c198747ffb163f1607cc1f
Victor Emanuel <Victor emanuel Vieira De Macedo>
2024-09-18 09:38:05 -0300

    UserController

--- jUnit-5-e-Mockito/src/main/java/api_rest_mockito/jUnit5/e/Mockito/controller/UserController.java
+++ jUnit-5-e-Mockito/src/main/java/api_rest_mockito/jUnit5/e/Mockito/controller/UserController.java
@@ -1,5 +1,7 @@
 package api_rest_mockito.jUnit5.e.Mockito.controller;
-import api_rest_mockito.jUnit5.e.Mockito.entity.User;
+import api_rest_mockito.jUnit5.e.Mockito.dto.UserDto;
+import api_rest_mockito.jUnit5.e.Mockito.dto.mapper.Mapper;
+import api_rest_mockito.jUnit5.e.Mockito.dto.mapper.MapperImpl;
 import api_rest_mockito.jUnit5.e.Mockito.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserController {
     @Autowired
+    private Mapper mapper;
+    @Autowired
     private UserService userService;
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User>findById(@PathVariable Long id){
-        return ResponseEntity.ok().body(userService.findByid(id));
+    public ResponseEntity<UserDto>findById(@PathVariable Long id){
+        UserDto dto = mapper.toDto(userService.findByid(id));
+        return ResponseEntity.ok().body(dto);
     }
 }
